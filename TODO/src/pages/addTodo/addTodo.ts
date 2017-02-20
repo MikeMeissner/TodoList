a079a84341aa0a2b5d677b940ddb0572e3a45520
MikeMeissner <tennisapb@gmail.com>
2017-02-20 13:38:18 -0500

    renamed pages

---
+++ TODO/src/pages/addTodo/addTodo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-addTodo',
+  templateUrl: 'addTodo.html'
+})
+export class AddTodoPage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
---
+++ TODO/src/pages/todos/todos.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-todos',
+  templateUrl: 'todos.html'
+})
+export class TodosPage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+
+}
